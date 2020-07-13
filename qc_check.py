d9666a2ba5d7b19e038fa664b2c8d909f673195f
svural <vurals@juno.cbio.private>
2020-07-13 15:10:18 -0400

    output Collab_IDs too

--- qc_check.py
+++ qc_check.py
@@ -20,8 +20,23 @@ def read_pairing_file(f):
             d[tumor_sample_id]=normal_sample_id
     return d
 
+def read_sample_data_clinical_file(f):
+    #d={sample{Collab_ID=...,Sample_ID=...},sample2{}}
+    d={}
+    with open(f) as sample_data_clinical_file:
+        sample_data_clinical_file.readline() #skip headerline
+        for line in sample_data_clinical_file:
+            line=line.rstrip().split()
+            Sample_ID =line[0]
+            Patient_ID=line[3]
+            Collab_ID =line[4]
+            if Sample_ID in d:
+                print 'error:Duplicate Sample_ID'
+                sys.exit(1)
+            d[Sample_ID]={'Patient_ID':Patient_ID,'Collab_ID':Collab_ID}
+    return d
 
-def check_homo_concordance(homo_concordance_file,pairing_file,match_threshold):
+def check_homo_concordance(clinical_data_dict,homo_concordance_file,pairing_file,match_threshold):
     print 'Checking homo_concordance'
 
     temp_file_name='tmp'
@@ -49,40 +64,40 @@ def check_homo_concordance(homo_concordance_file,pairing_file,match_threshold):
             same_patient_different_ids=[item for item in col_ids if row_sample_id in item] #s_C_LV79F0_N001_dZ ['s_C_LV79F0_X002_d', 's_C_LV79F0_X001_d']
             passed_threshold=[i for i,j in enumerate(values) if j >= match_threshold]
 
-            if 'FROZENPOOLEDNORMAL' in pair_id: #skip pooled normals
-                continue
+            # if pair_id=='s_FROZENPOOLEDNORMAL':
+            #     continue
 
             #didn't match anything, including its pair
             if len(passed_threshold)==0:
-                print row_id,"didn't match any genotype, including its pair",pair_id
+                print row_id+' ('+clinical_data_dict[row_id]['Collab_ID']+") didn't match any genotype, including its pair "+pair_id+' ('+clinical_data_dict[pair_id]['Collab_ID']+')'
 
             matched_col_ids=[col_ids[item] for item in passed_threshold]
 
             #didn't match its pair, but matched a different sample
             if len(passed_threshold)==1 and matched_col_ids[0] != pair_id :
-                print row_id,"didn't match its pair (",pair_id, "), instead matched a different sample", matched_col_ids[0]
+                print row_id+'('+clinical_data_dict[row_id]['Collab_ID']+") didn't match its pair ("+pair_id+"), instead matched a different sample"+matched_col_ids[0]+' ('+clinical_data_dict[matched_col_ids[0]]['Collab_ID']+')'
             if len(passed_threshold)==1 and matched_col_ids[0] != pair_id and matched_col_ids[0] in same_patient_different_ids :
-                print row_id,"didn't match its pair (",pair_id, "), instead matched a different sample from same patient", matched_col_ids[0]
+                print row_id,'(',clinical_data_dict[row_id]['Collab_ID']+") didn't match its pair ("+pair_id+"), instead matched a different sample from same patient"+matched_col_ids[0]+' ('+clinical_data_dict[matched_col_ids[0]]['Collab_ID']+')'
 
             #matched multiple genotypes
             if len(passed_threshold)>1:
                 if pair_id not in matched_col_ids:
-                    print row_id, "didn't match its pair, but matched other ids", matched_col_ids
+                    print row_id+'(',clinical_data_dict[row_id]['Collab_ID']+") didn't match its pair, but matched other ids"+matched_col_ids
                 else:
-                    print row_id, "matched its pair, but also matched other ids", matched_col_ids
+                    print row_id+'(',clinical_data_dict[row_id]['Collab_ID']+") matched its pair, but also matched other ids"+matched_col_ids
 
     output=delete_tmp_file('tmp')
     if output:
         print "error: couldn't delete the temp file"
         sys.exit(1)
 
-def check_cdna_contamination(cdna_contamination_file):
+def check_cdna_contamination(clinical_data_dict,cdna_contamination_file):
     print 'Checking cDNA contamination'
     f=open(cdna_contamination_file)
     if 'No detected cDNA contamination' in f.readline():
         print 'All good'
         return 0
-    d={}
+    d={} #d[sample_id]=[list of genes]
     for line in f:
         tumor_sample_barcode,hugo_symbol,count=line.rstrip().split()
         if tumor_sample_barcode not in d:
@@ -90,7 +105,8 @@ def check_cdna_contamination(cdna_contamination_file):
         d[tumor_sample_barcode].append(hugo_symbol)
 
     for item in d:
-        print item,'has cDNA contamination of', d[item]
+        print item+' ('+clinical_data_dict[item]['Collab_ID']+') has cDNA contamination of genes '+','.join(d[item])
+
 
     return 0
 
@@ -108,11 +124,9 @@ def main():
     pairing_file             =glob.glob(project_path+'/sample_pairing.txt')[0] #inputs/..._sample_pairing.txt
     sample_data_clinical_file=glob.glob(project_path+'/sample_data_clinical.txt')[0] #sample_data_clinical.txt
 
-
-    check_homo_concordance(homo_concordance_file,pairing_file,match_threshold)
-
-    check_cdna_contamination(cdna_contamination_file)
-
+    clinical_d=read_sample_data_clinical_file(sample_data_clinical_file)
+    check_homo_concordance(clinical_d,homo_concordance_file,pairing_file,match_threshold)
+    check_cdna_contamination(clinical_d,cdna_contamination_file)
 
 if __name__ == '__main__':
     main()
